d381b612ab6d51e62cb3c5eadff7610fd5229a08
Ariel Campos <ariel.campos94@yahoo.com>
2023-02-23 15:41:42 -0300

    boiler template

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    baseUrl: 'https://example.cypress.io',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
